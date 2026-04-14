43091c2fea314d986841c63c37763a9254bd7910
yoondahm <yoondahm@naver.com>
2026-04-15 00:50:14 +0900

    Feat many things

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!");
+initTheme();
+
+function initTheme() {
+    const btn = document.getElementById('theme-toggle-btn');
+    if (!btn) {
+        console.error("Theme toggle button not found");
+        return;
+    }
+    btn.addEventListener("click", function() {
+        document.body.classList.toggle("light-mode");
+    });
+}
