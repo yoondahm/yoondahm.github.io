1575650a97794000b0161ea442f5ef7facfec828
yoondahm <yoondahm@naver.com>
2026-04-15 12:30:52 +0900

    Added theme-change-switch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,26 @@
 initTheme();
 
 function initTheme() {
-    const btn = document.getElementById('theme-toggle-btn');
-    if (!btn) {
-        console.error("Theme toggle button not found");
-        return;
-    }
-    btn.addEventListener("click", function() {
-        document.body.classList.toggle("light-mode");
-    });
+  let defaultTheme = localStorage.getItem("theme");
+  if (!defaultTheme) {
+    defaultTheme = window.matchMedia("(prefers-color-scheme: light)").matches
+      ? "light"
+      : "dark";
+  }
+  document.body.classList.toggle("light-mode", defaultTheme === "light");
+
+  const themeSwitch = document.getElementById("theme-toggle");
+  if (!themeSwitch) {
+    console.error("Theme toggle switch not found");
+    return;
+  }
+  themeSwitch.classList.toggle("active", defaultTheme === "light");
+  themeSwitch.addEventListener("click", function () {
+    document.body.classList.toggle("light-mode");
+    localStorage.setItem(
+      "theme",
+      document.body.classList.contains("light-mode") ? "light" : "dark",
+    );
+    this.classList.toggle("active");
+  });
 }
